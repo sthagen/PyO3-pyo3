a3fc1895621241344ef66bd66bd31fab16e1cda1
sthagen <sthagen@users.noreply.github.com>
2022-07-13 18:13:13 +0000

    Upload documentation for main

---
+++ main/debug/build/serde_derive-518b4afc34b687d6/build_script_build-518b4afc34b687d6.d
@@ -0,0 +1,5 @@
+/home/runner/work/PyO3-pyo3/PyO3-pyo3/target/guide/debug/build/serde_derive-518b4afc34b687d6/build_script_build-518b4afc34b687d6: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/serde_derive-1.0.139/build.rs
+
+/home/runner/work/PyO3-pyo3/PyO3-pyo3/target/guide/debug/build/serde_derive-518b4afc34b687d6/build_script_build-518b4afc34b687d6.d: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/serde_derive-1.0.139/build.rs
+
+/home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/serde_derive-1.0.139/build.rs:
