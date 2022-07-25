669b84a18d06599e42bbde317c495bff7923b30c
sthagen <sthagen@users.noreply.github.com>
2022-07-25 19:10:22 +0000

    Upload internal documentation

--- internal/doc/pyo3/types/capsule/sidebar-items.js
+++ internal/doc/pyo3/types/capsule/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["capsule_destructor",""]],"struct":[["CapsuleContents",""],["PyCapsule","Represents a Python Capsule as described in Capsules:"]],"trait":[["AssertNotZeroSized","Guarantee `T` is not zero sized at compile time."]]};
+window.SIDEBAR_ITEMS = {"fn":[["capsule_destructor",""],["ensure_no_error",""]],"struct":[["CapsuleContents",""],["PyCapsule","Represents a Python Capsule as described in Capsules:"]],"trait":[["AssertNotZeroSized","Guarantee `T` is not zero sized at compile time."]]};
